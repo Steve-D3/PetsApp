62ec3b6bd930574c829bcd12375285b30161f530
Steven Duque Franco <steven.122@hotmail.es>
2025-06-20 16:39:39 +0200

    Prod

--- storage/framework/views/5c3b6c54668675ae37f163b591598f8d.php
+++ storage/framework/views/5c3b6c54668675ae37f163b591598f8d.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="<?php echo e(asset('favicon.ico')); ?>">
 
     <!-- Scripts -->
     <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
