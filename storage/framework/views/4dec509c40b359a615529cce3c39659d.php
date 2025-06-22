7a9b1d22587d77ec44c3bb3eefd11ff4a8c7e8e6
Steven Duque Franco <steven.122@hotmail.es>
2025-06-22 16:19:27 +0200

    fix: profile bug

--- storage/framework/views/4dec509c40b359a615529cce3c39659d.php
+++ storage/framework/views/4dec509c40b359a615529cce3c39659d.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
 
-        <!-- Scripts -->
-        <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="<?php echo e(asset('favicon.ico')); ?>">
 
-        <!-- Styles -->
-        <?php echo \Livewire\Mechanisms\FrontendAssets\FrontendAssets::styles(); ?>
+    <!-- Scripts -->
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
 
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-            <?php echo e($slot); ?>
+    <!-- Styles -->
+    <?php echo \Livewire\Mechanisms\FrontendAssets\FrontendAssets::styles(); ?>
 
-        </div>
+</head>
 
-        <?php echo \Livewire\Mechanisms\FrontendAssets\FrontendAssets::scripts(); ?>
+<body>
+    <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <?php echo e($slot); ?>
 
-    </body>
+    </div>
+
+    <?php echo \Livewire\Mechanisms\FrontendAssets\FrontendAssets::scripts(); ?>
+
+</body>
+
 </html>
 <?php /**PATH /var/www/html/resources/views/layouts/guest.blade.php ENDPATH**/ ?>
